f7ca38f44bc6f029e0b11ddb980f3b1105555be3
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-06-21 10:20:59 +0200

    [UPD] ampliamento foglio di stile

--- index.php
+++ index.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--CDN BOOTSTRAP-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- MY STYLE CSS-->
+    <link rel="stylesheet" href="./css/style.css">
     <title>PHP Dischi JSON</title>
 </head>
 
 <body>
-    <div id="app">
+    <div id="app" v-cloak>
         <header>
             <div class="container p-3 text-center">
                 <h1>CD musicali disponibili</h1>
@@ -21,7 +23,7 @@
                 <div class="row">
                     <div class="col-4" v-for="item in cd">
                         <div class="text-center p-3">
-                            <img style="width: 10rem;" :src="item.img" :alt="foto">
+                            <img :src="item.img" alt="foto">
                             <div>{{ item.name }}</div>
                             <div>{{ item.author }}</div>
                             <div><strong>{{ item.edition }}</strong></div>
--- server.php
+++ server.php
@@ -1,6 +1,5 @@
 <?php
 $list = file_get_contents(__DIR__ . '/list.json');
-//per trasformare array php in oggetti json
 header('Content-Type: application/json');
 // NON devo restituire una stringa con file JSON
 //echo  json_encode($list);
